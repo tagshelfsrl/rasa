1e23e400a66f81a8d889ee7cf0ffe2070c9ca09f
Yuyi Pacheco Kimura <yuyik46@gmail.com>
2020-05-19 19:34:49 -0400

    Added reset slot as default in tracker (#3)

--- rasa/core/domain.py
+++ rasa/core/domain.py
@@ -27,6 +27,7 @@ from rasa.core.constants import (
     DEFAULT_KNOWLEDGE_BASE_ACTION,
     REQUESTED_SLOT,
     FORM_ID_SLOT,
+    RESET_SLOT,
     SLOT_LAST_OBJECT,
     SLOT_LAST_OBJECT_TYPE,
     SLOT_LISTED_ITEMS,
@@ -411,6 +412,14 @@ class Domain:
         """
         if self.form_names and FORM_ID_SLOT not in [s.name for s in self.slots]:
             self.slots.append(UnfeaturizedSlot(FORM_ID_SLOT))
+
+    def add_reset_slot(self) -> None:
+        """Add a slot called `form_id` to the list of slots.
+
+        The value of this slot will hold a unique identifier for the active forms.
+        """
+        if self.form_names and RESET_SLOT not in [s.name for s in self.slots]:
+            self.slots.append(UnfeaturizedSlot(RESET_SLOT))
 
     def add_knowledge_base_slots(self) -> None:
         """
